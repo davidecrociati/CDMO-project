56cc6fb538324ac8deac366fb9c35eeb7e935cbb
drunkyyy <90336130+davidesonno@users.noreply.github.com>
2024-05-11 19:27:47 +0200

    launcher

--- CP/CP_launcher.py
+++ CP/CP_launcher.py
@@ -3,13 +3,33 @@ import os
 
 def solve_mzn(mzn_file, dzn_file):
     # Load the MiniZinc model
-    model = pymzn.minizinc(mzn_file)
+    types={
+        "num_couriers": {"type" : "int"},
+        "num_items": {"type" : "int"},
+        # "COURIERS": {"type" : "int", "set" : true},
+        "courier_sizes": {"type" : "int", "dim" : 1, "dims" : ["int"]},
+        # "ITEMS": {"type" : "int", "set" : true},
+        "item_sizes": {"type" : "int", "dim" : 1, "dims" : ["int"]},
+        # "NODES": {"type" : "int", "set" : true},
+        "distances": {"type" : "int", "dim" : 2, "dims" : ["int","int"]}
+   }
+    
+    # types = {
+    #     "num_couriers": 'int',
+    #     "num_items": 'int',
+    #     "courier_sizes": 'array[int] of int',
+    #     "item_sizes": 'array[int] of int',
+    #     "distances": 'array[int, int] of int'
+    # }
+    
+    data = pymzn.dzn2dict(dzn_file,types=types)
+    # print(data)
+    # model = pymzn.minizinc(mzn_file)
 
     # Load the MiniZinc data file
-    data = pymzn.dzn_load(dzn_file)
 
     # Solve the MiniZinc model with the provided data
-    result = pymzn.minizinc(model, data=data, output_mode='json')
+    result = pymzn.minizinc(mzn_file, data=data, output_mode='json')
 
     return result
 
