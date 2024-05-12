e15c7a6f5a8cd54f19a553525fbaa3a49a66263e
drunkyyy <90336130+davidesonno@users.noreply.github.com>
2024-05-12 16:19:16 +0200

    a

--- CP/CP_launcher.py
+++ CP/CP_launcher.py
@@ -13,7 +13,6 @@ def solve_mzn(mzn_file, dzn_file):
     # Create an Instance of the n-Queens model for Gecode
     instance = Instance(gecode, model)
     # Assign 4 to n
-    # instance["n"] = 4
     result = instance.solve()
     # Output the array q
     return result
@@ -24,7 +23,7 @@ if __name__=='__main__':
 
     os.chdir(script_dir)
     # Example usage
-    mzn_file = 'toy_solver.mzn'
+    mzn_file = 'solver.mzn'
     dzn_file = '../instances_dzn/inst01.dzn'
     solution = solve_mzn(mzn_file, dzn_file)
     print(solution)
