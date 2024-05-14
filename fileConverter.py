fc56f18f9069c2d12bf7bed70d59781d79ba0312
TheNewPelle <76878171+TheNewPelle@users.noreply.github.com>
2024-05-14 18:39:31 +0200

    upper_bound e lower_bound

--- fileConverter.py
+++ fileConverter.py
@@ -8,19 +8,38 @@ def insert_commas(string:str):
     formatted_string = ", ".join(elements)
     return formatted_string
 
+def matrix_parser(string_matrix):
+    rows = [row.strip().split() for row in string_matrix]
+    
+    matrix = [[] for _ in range(len(rows))]
+    
+    for i in range(len(rows)):
+        for j in range(len(rows[i])):
+            matrix[i].append(int(rows[i][j]))
+    
+    return matrix
 
-def write_new_content(data, variables):
+def find_upper_bound(data):
+    return sum([max(r) for r in data])
+
+def find_lower_bound(data):
+    return 0
+
+def write_new_content(data):
     content = ''
-    content += variables[0]+'='+data[0]+';\n'
-    content += variables[1]+'='+data[1]+';\n'
-    content += variables[2]+'=['+insert_commas(data[2])+'];\n'
-    content += variables[3]+'=['+insert_commas(data[3])+'];\n'
-    content+=variables[4]+'=['
+    bounds = matrix_parser(data[4:]) 
+    
+    content += 'num_couriers='+data[0]+';\n'
+    content += 'num_items='+data[1]+';\n'
+    content += 'courier_sizes=['+insert_commas(data[2])+'];\n'
+    content += 'item_sizes=['+insert_commas(data[3])+'];\n'
+    content += 'lower_bound='+str(find_lower_bound(bounds))+';\n'
+    content += 'upper_bound='+str(find_upper_bound(bounds))+';\n'
+    content += 'distance=['
     for table_row in data[4:]:
         content+='|'+insert_commas(table_row)+'\n'
     content=content[:-1]
     content+='|];\n'
-    
     return content
 
 def read_file(path):
@@ -51,15 +70,13 @@ if __name__=='__main__':
     DZN_instances_folder = "instances_dzn"
     os.makedirs(DZN_instances_folder, exist_ok=True)
     file_names = os.listdir(instances_folder)
-    
-    variables=['num_couriers','num_items','courier_sizes','item_sizes','distances']
-    
+        
     for file_name in file_names:
         if file_name.endswith(".dat"):
             file_path = os.path.join(instances_folder, file_name)
 
             data=read_file(file_path)
-            formatted_data=write_new_content(data,variables)
+            formatted_data=write_new_content(data)
 
             dzn_file_name=DZN_instances_folder+'/'+file_name[:-4]
             save_dzn_file(dzn_file_name,formatted_data)
