83b3334cff75040308ba000e84067329d50ba852
drunkyyy <90336130+davidesonno@users.noreply.github.com>
2024-05-15 10:52:28 +0200

    changed some file names and prepared a template to launch the search

--- CP/CP_launcher.py
+++ CP/CP_launcher.py
@@ -2,19 +2,39 @@ from minizinc import Instance, Model, Solver
 from datetime import timedelta
 import os
 
-def solve_mzn(mzn_file, dzn_file):
-    # Load n-Queens model from file
+def solve_mzn(mzn_file, dzn_file,solver,params):
     model = Model(mzn_file)
 
     model.add_file(dzn_file)
-    # Find the MiniZinc solver configuration for Gecode
-    solver = Solver.lookup("chuffed")
-    # Create an Instance of the n-Queens model for Gecode
+
+    solver = Solver.lookup(solver)
+
     instance = Instance(solver, model)
-    # Assign 4 to n
-    result = instance.solve(timeout=timedelta(seconds=300))
-    # Output the array q
+    # TODO fargli passare i parametri
+    result = instance.solve(timeout=timedelta(seconds=15))
+
     return result
+
+def launch_CP(instances:list,model:str='model.mzn',solver:str='chuffed',params:dict={}):
+    '''
+    Parameters:
+    instances (list): A list of file paths to the instance files to be solved.
+    model (str): The file path to the MiniZinc model file.
+    solver (str): The solver to use for solving the instances.
+    params (dict): Additional parameters to pass to the solver.
+
+    Returns:
+    dict: A dictionary where keys are instance file paths and values are the solutions.
+    '''
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
+
+    results={} # bho tocca in qualche modo ritornare/salvare i risultati
+    for instance in instances[:]:
+        solution = solve_mzn(model, instance,solver,params)
+        results[instance]=solution
+        # TODO il json
+    return results
 
 if __name__=='__main__':
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,9 +42,9 @@ if __name__=='__main__':
 
     instances_folder='../instances_dzn'
     instances=[instances_folder+'/'+instance for instance in os.listdir(instances_folder) if instance.endswith('.dzn')]
-    mzn_file = 'solver.mzn'
+    mzn_file = 'model.mzn'
 
-    for instance in instances[15:16]:
-        solution = solve_mzn(mzn_file, instance)
+    for instance in instances[:1]:
+        print(f'Solving {instance[len(instances_folder)+1:]}...')
+        solution = solve_mzn(mzn_file, instance,'chuffed')
         print(solution)
-        # TODO il json
---
+++ main.py
@@ -0,0 +1,4 @@
+from CP.CP_launcher import launch_CP
+# deve eseguire cp_launcher.launch_CP con gli adeguati parametri
+# e poi potenzialmente prendere tutti i risultati e salvarli su re/CP/ ????
+# etc
