ff5924cb1510a2ac8d5f64456a0ce182d9179c17
TheNewPelle <76878171+TheNewPelle@users.noreply.github.com>
2024-05-19 11:13:53 +0200

    primi encodings + requirements

---
+++ SAT/encodings.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+from z3 import *
+
+def at_least_one(bool_vars):
+    return Or(bool_vars)
+
+def at_most_one(bool_vars):
+    return [Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)]
+
+def exactly_one(bool_vars):
+    return at_most_one(bool_vars) + [at_least_one(bool_vars)]
