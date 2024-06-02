811e965a563a5fcd8b814aed98bec6dfe20bca43
Davide Crociati <47207765+davidecrociati@users.noreply.github.com>
2024-06-02 19:12:52 +0200

    Update MIP_solver.py

--- MIP/MIP_solver.py
+++ MIP/MIP_solver.py
@@ -24,11 +24,109 @@ def set_constraints(prob, data):
     upper_bound = data['upper_bound']
     
     # Decision variables
-    stops = [[LpVariable(name=f'stops{i}_{j}', 
+    stops = [[LpVariable(name=f'stops_{i}_{j}', 
                          cat=LpInteger,
                          lowBound=0,
-                         upBound=num_items+1)
+                         upBound=num_items)
               for j in range(num_items)] for i in range(num_couriers)]
+    courier_loads = [LpVariable(name=f'courier_loads_{c}', lowBound=0, upBound=courier_capacities[c], cat=LpInteger) for c in range(num_couriers)]
+    item_resp = [LpVariable(name=f'item_resp_{c}', lowBound=0, upBound=num_items -1, cat=LpInteger) for c in range(num_couriers)]
+    distances_travelled = [LpVariable(name=f'distances_travelled_{c}', lowBound=lower_bound, upBound=upper_bound, cat=LpInteger) for c in range(num_couriers)]
+    successors = [LpVariable(name=f'successors_{i}', lowBound=0, upBound=num_items, cat=LpInteger) for i in range(num_items)]
+    # Objective function
+    longest_trip = LpVariable(name=f'longest_trip', lowBound=lower_bound, upBound=upper_bound, cat=LpInteger)
     
-    # prob += ...
-    pass
+    # Define courier loads
+    for c in range(num_couriers):
+        prob += courier_loads[c] == lpSum(item_sizes[i] for i in range(num_items) if item_resp[i]==c)
+        
+    # capacity constraint: the sum of item sizes assigned to each courier must not exceed the courier's capacity
+    for c in range (num_couriers):
+        prob += courier_loads[c] <= courier_capacities[c]
+    
+    # each courier must be responsible for delivering at least one item
+    for c in range(num_couriers):
+        prob += lpSum(item_resp[i] == c for i in range(num_items)) >= 1
+    
+    # each courier have to stop in at least one location different from the depot
+    for c in range(num_couriers):
+        prob += stops[c][0] < num_items
+        
+    # all items must be delivered
+    for i in range(num_items):
+        prob += lpSum(stops[c][i2]==i for c in range(num_couriers) for i2 in range(num_items)) == 1
+    
+    # Channeling constraint
+    for c in range(num_couriers):
+        pass
+    
+    return (num_couriers,
+            num_items,
+            stops,
+            courier_loads,
+            item_resp,
+            distances_travelled,
+            successors,
+            longest_trip)
+        
+def set_constraints_2(prob, data):
+    num_couriers = data['num_couriers']
+    num_items = data['num_items']
+    courier_capacities = data['courier_capacities']
+    item_sizes = data['item_sizes']
+    distances = data['distances']
+    lower_bound = data['lower_bound']
+    upper_bound = data['upper_bound']
+        
+    for num_couriers in range(1,num_couriers+1):
+        
+        # definition of LpProblem instance
+        problem = pulp.LpProblem("CVRP", pulp.LpMinimize)
+
+        # definition of variables which are 0/1
+        x = [[[pulp.LpVariable("x%s_%s,%s"%(i,j,k), cat="Binary") if i != j else None for k in range(num_couriers)]for j in range(num_items)] for i in range(num_items)]
+
+        # add objective function
+        problem += pulp.lpSum(distances[i][j] * x[i][j][k] if i != j else 0
+                            for k in range(num_couriers) 
+                            for j in range(num_items) 
+                            for i in range (num_items))
+
+        # constraints
+        # foluma (2)
+        for j in range(1, num_items):
+            problem += pulp.lpSum(x[i][j][k] if i != j else 0 
+                                for i in range(num_items) 
+                                for k in range(num_couriers)) == 1 
+
+        # foluma (3)
+        for k in range(num_couriers):
+            problem += pulp.lpSum(x[0][j][k] for j in range(1,num_items)) == 1
+            problem += pulp.lpSum(x[i][0][k] for i in range(1,num_items)) == 1
+
+        # foluma (4)
+        for k in range(num_couriers):
+            for j in range(num_items):
+                problem += pulp.lpSum(x[i][j][k] if i != j else 0 
+                                    for i in range(num_items)) -  pulp.lpSum(x[j][i][k] for i in range(num_items)) == 0
+
+        #foluma (5)
+        for k in range(num_couriers):
+            problem += pulp.lpSum(df.demand[j] * x[i][j][k] if i != j else 0 for i in range(num_items) for j in range (1,num_items)) <= courier_capacities[k] 
+
+
+        # fomula (6)
+        subtours = []
+        for i in range(2,num_items):
+            subtours += itertools.combinations(range(1,num_items), i)
+
+        for s in subtours:
+            problem += pulp.lpSum(x[i][j][k] if i !=j else 0 for i, j in itertools.permutations(s,2) for k in range(num_couriers)) <= len(s) - 1
+
+        
+        # print num_couriers which needed for solving problem
+        # print calculated minimum distance value
+        if problem.solve() == 1:
+            print('Vehicle Requirements:', num_couriers)
+            print('Moving Distance:', pulp.value(problem.objective))
+            break
